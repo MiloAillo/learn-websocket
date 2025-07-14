ad4e49571b2eeffec5b6416fadaa7b25f16d0ca5
Mischiko Moe <panuwucake@gmail.com>
2025-07-14 08:44:22 +0700

    basic client & server connection

--- public/client.js
+++ public/client.js
@@ -1,3 +1,5 @@
-import { io } from "socket.io-client";
-
+import { io } from "https://cdn.socket.io/4.8.1/socket.io.esm.min.js";
 const socket = io('http://localhost:3000')
+socket.on('connect', () => {
+    console.log(socket.id)
+})
--- server.js
+++ server.js
@@ -1,4 +1,9 @@
-const io = require("socket.io")(3000)
+const cors = require("cors")
+const io = require("socket.io")(3000, {
+    cors: {
+        origin: ["http://127.0.0.1:5500"]
+    }
+})
 
 io.on('connection', socket => {
     console.log(`${socket.id} is sucesfully connected!`)
